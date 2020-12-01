344337f72856e8b1a84c1a09c92d89d3a7f8ee02
Mikhail Zhigun <miz.github@gmail.com>
2020-12-01 21:18:13 +0100

    Added cmake-based build system

---
+++ cmake/compiler/gnu.cmake
@@ -0,0 +1,17 @@
+if(__claw_base_compiler)
+  return()
+endif()
+set(__claw_base_compiler YES)
+
+# For GNU compiler
+set(FC_BASIC_FLAGS "-fconvert=big-endian -fno-range-check")
+set(FC_FPPFLAGS "-cpp") 
+set(FC_RELEASE_OPTFLAGS "-O3")
+set(FC_DEBUG_OPTFLAGS "-O0")
+set(FC_RELEASE_WARNFLAGS "-Wall -Wno-unused-label -Wno-unused-dummy-argument -Wno-unused-variable")
+set(FC_DEBUG_WARNFLAGS "-Wall")
+set(FC_RELEASE_DEBUGFLAGS "-g -ffpe-trap=invalid,zero,overflow")
+set(FC_DEBUG_DEBUGFLAGS "-g -ffpe-trap=invalid,zero,overflow -fcheck=bounds -finit-real=snan -fbacktrace")
+set(FC_OMPFLAG "-fopenmp")
+set(FC_MODFLAG "-J")
+
---
+++ cmake/compiler/intel.cmake
@@ -0,0 +1,17 @@
+if(__claw_base_compiler)
+  return()
+endif()
+set(__claw_base_compiler YES)
+
+# For Intel compiler
+set(FC_BASIC_FLAGS "-convert big_endian -fpe0 -fp-model precise -ftz -fp-speculation safe")
+set(FC_FPPFLAGS "") 
+set(FC_RELEASE_OPTFLAGS "-O3")
+set(FC_DEBUG_OPTFLAGS "-O0")
+set(FC_RELEASE_WARNFLAGS "-warn all")
+set(FC_DEBUG_WARNFLAGS "${FC_RELEASE_WARNFLAGS}")
+set(FC_RELEASE_DEBUGFLAGS "-g")
+set(FC_DEBUG_DEBUGFLAGS "${FC_RELEASE_DEBUGFLAGS}")
+set(FC_OMPFLAG "-qopenmp -qopenmp-lib=compat")
+set(FC_MODFLAG "-module")
+
